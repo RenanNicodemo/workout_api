8ee4494e18b2098c41bdc461e278754d3d162997
RenanNicodemo <50030671+RenanNicodemo@users.noreply.github.com>
2025-08-31 22:54:39 -0300

    Update settings.py

--- workout_api/configs/settings.py
+++ workout_api/configs/settings.py
@@ -7,14 +7,14 @@ class Settings(BaseSettings):
     Ela lê as variáveis de um arquivo .env.
     """
     model_config = SettingsConfigDict(
-        env_file='.env',   # Especifica o nome do arquivo de onde ler as variáveis
-        extra='ignore'     # Ignora variáveis extras que não foram definidas aqui
+        env_file='.env',   
+        extra='ignore'     
     )
 
-    DATABASE_URL: str  # Define que a variável DATABASE_URL é esperada e deve ser uma string
+    DATABASE_URL: str  
     API_SECRET_KEY: str
     API_ALGORITHM: str
 
 
-# Cria uma instância única das configurações para ser usada em todo o projeto
+
 settings = Settings()
